94d0f834fae2a1c6cd97f5d15f5ea57cf7977be9
Alex Knowles <alexk@datalogics.com>
2017-02-23 11:46:05 -0600

    Print time and date of game starting.

--- studio_game.rb
+++ studio_game.rb
@@ -18,6 +18,11 @@ msg_width4 = player_name4.length + 27
 player_status_msg4 = "#{player_name4.capitalize.ljust(msg_width4, '.')} #{player_health4} health"
 puts player_status_msg4
 
+t = Time.now
+date_str = t.strftime("%A %m/%d/%Y")
+time_str = t.strftime("%I:%M%p")
+puts "The game started on #{date_str} at #{time_str}"
+
 puts ""
 puts "Players:"
 puts "\t#{player_name1}"
