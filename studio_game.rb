f4d8751d6195ee69fb335bc92f195cb01cb2f91c
Alex Knowles <alexk@datalogics.com>
2017-02-23 10:54:51 -0600

    Print player 3 status using String.center.

--- studio_game.rb
+++ studio_game.rb
@@ -4,9 +4,13 @@ player_name3 = "moe"
 
 player_health1 = 60
 player_health2 = 125
+player_health3 = 100
 
 puts "#{player_name1.capitalize} has a health of #{player_health1}."
 puts "#{player_name2.upcase} has a health of #{player_health2}."
+
+player_status_msg3 = "#{player_name3.capitalize} has a health of #{player_health3}."
+puts player_status_msg3.center(player_status_msg3.length + 13 * 2, '*')
 
 
 puts ""
