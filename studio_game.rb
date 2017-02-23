9ae99b59e7696244f3e0c3be2377a8ce059abdce
Alex Knowles <alexk@datalogics.com>
2017-02-23 13:21:32 -0600

    Clear out puts calls.

--- studio_game.rb
+++ studio_game.rb
@@ -14,12 +14,5 @@ class Player
 end
 
 player1 = Player.new("moe")
-puts player1.inspect
-puts player1
-puts player1.say_hello
-
 player2 = Player.new("larry", 60)
-puts player2.say_hello
-
 player3 = Player.new("curly", 125)
-puts player3.say_hello
