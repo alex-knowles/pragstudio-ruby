c8830cf56fa1c43439b4c257c5a3543fa59f3f8c
Alex Knowles <alexk@datalogics.com>
2017-02-23 13:11:58 -0600

    Override to_s method for Player.

--- studio_game.rb
+++ studio_game.rb
@@ -3,6 +3,11 @@ class Player
     @name = name
     @health = health
   end  
+
+  def to_s
+    "#{@name.capitalize} has a health of #{@health}."
+  end
 end
 
 player1 = Player.new("larry", 90)
+puts player1
