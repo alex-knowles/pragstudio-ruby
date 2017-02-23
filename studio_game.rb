5b67fdf43298b13c60a782a1cdad6c1e4a076f13
Alex Knowles <alexk@datalogics.com>
2017-02-23 13:37:35 -0600

    Clear out print statements from exercise 7.

--- studio_game.rb
+++ studio_game.rb
@@ -22,10 +22,3 @@ end
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
-puts player1
-puts player2
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
