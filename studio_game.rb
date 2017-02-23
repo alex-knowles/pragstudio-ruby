95d9c303dfe7544be87e9c19ce894faabe9ba210
Alex Knowles <alexk@datalogics.com>
2017-02-23 13:14:46 -0600

    Capitalize name param in Player#initialize.

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,11 @@
 class Player
   def initialize(name, health)
-    @name = name
+    @name = name.capitalize
     @health = health
   end  
 
   def to_s
-    "#{@name.capitalize} has a health of #{@health}."
+    "#{@name} has a health of #{@health}."
   end
 end
 
