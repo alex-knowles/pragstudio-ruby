e5c137f2b35997e753ba5676b8a25a22b70ebad0
Alex Knowles <alexk@datalogics.com>
2017-02-23 10:02:45 -0600

    Divide health by 9 with and without decimals.

--- studio_game.rb
+++ studio_game.rb
@@ -1,5 +1,8 @@
 player_name = "larry"
 player_health = 60
 
-player_status_msg = "#{player_name}'s health is #{player_health * 3}"
+player_status_msg = "#{player_name}'s health is #{player_health / 9}"
 puts player_status_msg
+player_status_msg = "#{player_name}'s health is #{player_health / 9.0}"
+puts player_status_msg
+
