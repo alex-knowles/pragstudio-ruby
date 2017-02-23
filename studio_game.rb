223e88bca969d358bb3f6184f41effbdf76ca5ae
Alex Knowles <alexk@datalogics.com>
2017-02-23 12:44:54 -0600

    Accept health param in say_hello.

--- studio_game.rb
+++ studio_game.rb
@@ -1,8 +1,8 @@
-def say_hello(name)
-  "I'm #{name.capitalize}!"
+def say_hello(name, health)
+  "I'm #{name.capitalize} with a health of #{health}."
 end
 
-puts say_hello("larry")
-puts say_hello("curly")
-puts say_hello("moe")
-puts say_hello("shemp")
+puts say_hello("larry", 60)
+puts say_hello("curly", 125)
+puts say_hello("moe", 100)
+puts say_hello("shemp", 90)
