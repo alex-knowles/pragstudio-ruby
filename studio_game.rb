c41aaa265257d7210303a460de41de7cb6dea4d9
Alex Knowles <alexk@datalogics.com>
2017-02-23 10:09:03 -0600

    Print indented player names.

--- studio_game.rb
+++ studio_game.rb
@@ -3,3 +3,9 @@ player_health = 60
 
 player_status_msg = "#{player_name}'s health is #{player_health}"
 puts player_status_msg
+
+puts ""
+puts "Players:"
+puts "\tlarry"
+puts "\tcurly"
+puts "\tmoe"
