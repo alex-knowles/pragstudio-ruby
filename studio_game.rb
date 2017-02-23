0a1d9865b3ec4dd49630169f2220f8c369e22362
Alex Knowles <alexk@datalogics.com>
2017-02-23 13:18:20 -0600

    Define Player#say_hello.

--- studio_game.rb
+++ studio_game.rb
@@ -7,8 +7,13 @@ class Player
   def to_s
     "#{@name} has a health of #{@health}."
   end
+
+  def say_hello
+    "I'm #{@name} with a health of #{@health}."
+  end
 end
 
 player1 = Player.new("moe")
 puts player1.inspect
 puts player1
+puts player1.say_hello
