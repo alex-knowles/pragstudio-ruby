a5ad0f1ccec401e3c79b571caf8e58b17ad866b5
Alex Knowles <alexk@datalogics.com>
2017-02-23 10:10:41 -0600

    Refactor name printing using variables.

--- studio_game.rb
+++ studio_game.rb
@@ -1,4 +1,7 @@
 player_name1 = "larry"
+player_name2 = "curly"
+player_name3 = "moe"
+
 player_health1 = 60
 
 player_status_msg = "#{player_name1}'s health is #{player_health1}"
@@ -6,6 +9,7 @@ puts player_status_msg
 
 puts ""
 puts "Players:"
-puts "\tlarry"
-puts "\tcurly"
-puts "\tmoe"
+puts "\t#{player_name1}"
+puts "\t#{player_name2}"
+puts "\t#{player_name3}"
+
