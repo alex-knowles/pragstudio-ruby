bb97323b994fd8a34b2db93a8c0d9a5057933053
Alex Knowles <alexk@datalogics.com>
2017-02-23 13:24:40 -0600

    Define Player#blam.

--- studio_game.rb
+++ studio_game.rb
@@ -11,8 +11,16 @@ class Player
   def say_hello
     "I'm #{@name} with a health of #{@health}."
   end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
 end
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
+puts player3.say_hello
+player3.blam
+puts player3.say_hello
