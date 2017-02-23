9ebf0a1c3b1fbd8643815d4d3ef40c50194c5ad3
Alex Knowles <alexk@datalogics.com>
2017-02-23 10:00:17 -0600

    Refactor with a double quoted string.

--- studio_game.rb
+++ studio_game.rb
@@ -1,5 +1,5 @@
 player_name = "larry"
 player_health = 60
 
-player_status_msg = player_name + '\'s health is ' + player_health.to_s
+player_status_msg = "#{player_name}'s health is #{player_health}"
 puts player_status_msg
