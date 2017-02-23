e579744ae717181b4baf33a74a306a18fa6423e5
Alex Knowles <alexk@datalogics.com>
2017-02-23 13:38:36 -0600

    Make Player’s health read-accessible.

--- studio_game.rb
+++ studio_game.rb
@@ -4,6 +4,8 @@ class Player
     @health = health
   end  
 
+  attr_reader :health
+
   def to_s
     "I'm #{@name} with a health of #{@health}."
   end
@@ -22,3 +24,7 @@ end
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
+
+puts player1.health
+puts player2.health
+puts player3.health
