03ae9d1d4863e13d9d42a98ba1938a2510589610
Alex Knowles <alexk@datalogics.com>
2017-02-23 12:49:27 -0600

    Define time method.

--- studio_game.rb
+++ studio_game.rb
@@ -1,5 +1,10 @@
+def time
+  t = Time.now
+  t.strftime("%H:%M:%S")
+end
+
 def say_hello(name, health=100)
-  "I'm #{name.capitalize} with a health of #{health}."
+  "I'm #{name.capitalize} with a health of #{health} as of #{time}."
 end
 
 puts say_hello("larry", 60)
