1e5e8ed587feedff69719337012f6daaee8ae475
Alex Knowles <alexk@datalogics.com>
2017-02-24 07:14:39 -0600

    Factor out a GameTurn module from Player#play.
    Remove some complexity from the play method. Create
    a GameTurn module that is responsible for die rolls
    and w00ting, blamming, or skipping Players.

---
+++ game_turn.rb
@@ -0,0 +1,18 @@
+require_relative 'die'
+require_relative 'player'
+
+module GameTurn
+
+  def self.take_turn(player)
+    die = Die.new
+    case die.roll
+      when 1..2
+	player.blam
+      when 5..6
+        player.w00t
+      else
+       #skip player
+    end
+  end
+
+end
