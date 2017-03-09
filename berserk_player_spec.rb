874420927711190682baa0598ce2b6bfb23ab44a
Alex Knowles <alexk@datalogics.com>
2017-03-09 08:14:53 -0600

    Define spec for BerserkPlayer.
    Like a normal Player, except that it goes into
    “berserk” mode after 5 w00ts.  In “berserk” mode,
    blams act as w00ts.

---
+++ berserk_player_spec.rb
@@ -0,0 +1,22 @@
+require_relative 'berserk_player'
+
+describe BerserkPlayer do
+
+  before do
+    @initial_health = 50
+    @player = BerserkPlayer.new("berserker", @initial_health)
+  end
+
+  it "gets health increases from blams after 6 w00ts" do
+    expect(@player.health).to eq(@initial_health)
+
+    6.times { @player.w00t }
+    expected_health = @initial_health + 6 * 15
+    expect(@player.health).to eq(expected_health)
+
+    2.times { @player.blam }
+    expected_health += 2 * 15
+    expect(@player.health).to eq(expected_health)
+  end
+
+end
