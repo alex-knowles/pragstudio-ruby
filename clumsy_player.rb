60ca73590e99f48e2c829d36992591e4e9d945ca
Alex Knowles <alexk@datalogics.com>
2017-03-08 23:04:06 -0600

    Refactor ClumsyPlayer#found_treasure.
    Remove duplicated code by calling ‘super’ method.

--- clumsy_player.rb
+++ clumsy_player.rb
@@ -3,10 +3,7 @@ require_relative 'player'
 class ClumsyPlayer < Player
 
   def found_treasure(treasure)
-    points = treasure.points / 2.0
-    @found_treasures[treasure.name] += points
-    puts "#{@name} found a #{treasure.name} worth #{points} points."
-    puts "#{@name}'s treasures: #{@found_treasures}"
+    super(Treasure.new(treasure.name, treasure.points / 2.0))
   end
 
 end
