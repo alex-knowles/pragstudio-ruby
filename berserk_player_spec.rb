5b304a2a81aab7bcaa1bb001977972421362a1f8
Alex Knowles <alexk@datalogics.com>
2017-03-09 08:40:40 -0600

    Remove complicated test.
    It is replaced by 3 specific ones.

--- berserk_player_spec.rb
+++ berserk_player_spec.rb
@@ -27,20 +27,4 @@ describe BerserkPlayer do
     expect(@player.health).to eq(expected_health)
   end
 
-  it "gets health increases from blams after 6 w00ts" do
-    expect(@player.health).to eq(@initial_health)
-
-    @player.blam
-    expected_health = @initial_health - 10
-    expect(@player.health).to eq(expected_health)
-
-    6.times { @player.w00t }
-    expected_health += 6 * 15
-    expect(@player.health).to eq(expected_health)
-
-    2.times { @player.blam }
-    expected_health += 2 * 15
-    expect(@player.health).to eq(expected_health)
-  end
-
 end
