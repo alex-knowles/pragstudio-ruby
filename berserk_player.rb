86ec8700a6ce05698296c6c13349163d670f882e
Alex Knowles <alexk@datalogics.com>
2017-03-09 08:30:48 -0600

    Implement berserk mode in BerserkPlayer.
    After 6 w00ts, blams have the same effect as
    w00ts.

--- berserk_player.rb
+++ berserk_player.rb
@@ -2,4 +2,28 @@ require_relative 'player'
 
 class BerserkPlayer < Player
 
+  def initialize(name, health=100)
+    super(name, health)
+    @w00t_count = 0
+  end
+
+  def w00t
+    @w00t_count += 1
+    super
+  end
+
+  def blam
+    if berserk?
+      w00t
+    else
+      super
+    end
+  end
+
+private
+
+  def berserk?
+    @w00t_count >= 5
+  end
+
 end
--- berserk_player_spec.rb
+++ berserk_player_spec.rb
@@ -10,8 +10,12 @@ describe BerserkPlayer do
   it "gets health increases from blams after 6 w00ts" do
     expect(@player.health).to eq(@initial_health)
 
+    @player.blam
+    expected_health = @initial_health - 10
+    expect(@player.health).to eq(expected_health)
+
     6.times { @player.w00t }
-    expected_health = @initial_health + 6 * 15
+    expected_health += 6 * 15
     expect(@player.health).to eq(expected_health)
 
     2.times { @player.blam }
